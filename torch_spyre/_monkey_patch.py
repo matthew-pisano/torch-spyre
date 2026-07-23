1d752e21cf52fa8dbd95bf80aac096dda41f4920
matthew-pisano <matthewpisano14@gmail.com>
2026-07-23 13:38:51 -0400

    Handle When Size is Positional and Kwarg
    Signed-off-by: matthew-pisano <matthewpisano14@gmail.com>

--- torch_spyre/_monkey_patch.py
+++ torch_spyre/_monkey_patch.py
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 
-from torch_spyre.constants import DEVICE_NAME
-
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from torch._dynamo.guards import GuardBuilder
+
+from torch_spyre.constants import DEVICE_NAME
 
 if TYPE_CHECKING:
     from torch_spyre._C import SpyreTensorLayout
@@ -174,8 +174,13 @@ def _patch_tensor_for_spyre():
     ):
         # torch.empty supports size as either a positional arg or keyword arg.
         # Normalise so downstream always receives it as positional.
-        if size is not None and not args:
+        if size is not None:
+            if args:
+                raise TypeError(
+                    "empty() received an invalid combination of arguments - got (tuple, size=tuple)"
+                )
             args = (size,)
+
         if (
             device_layout is None
         ):  # use original implementation if no layout is provided
